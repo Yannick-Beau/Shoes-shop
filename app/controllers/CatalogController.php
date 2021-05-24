09590207c5910d16fad1c8b54c1a222200dc0076
Yannick-Beau <beauyannick@gmail.com>
2021-05-24 15:12:40 +0200

    import 24/05/2021 from e03-routing-et-bdd

---
+++ app/controllers/CatalogController.php
@@ -0,0 +1,24 @@
+<?php
+
+    class CatalogController
+    {
+        public function category( $routeVarInfos ) 
+        {
+            dump( $routeVarInfos );
+
+            // L'id de la catégorie que je souhaite afficher est donc stocké dans
+            dump( $routeVarInfos['id'] );
+
+            $this->show( "product.list" );
+        }
+
+        // Cette méthode n'est appellée QUE par le controleur via ses propres méthodes
+        // et jamais depuis l'exéterieur, on peut la définir comme private
+        private function show( $viewName, $viewVars = [] ) 
+        {
+            // $viewVars est disponible dans chaque fichier de vue
+            require_once __DIR__.'/../views/partials/header.tpl.php';
+            require_once __DIR__.'/../views/'.$viewName.'.tpl.php';
+            require_once __DIR__.'/../views/partials/footer.tpl.php';
+        }
+    }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,60 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e93b559081c8af062d0de98b0fbe76f9cfe68730',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e93b559081c8af062d0de98b0fbe76f9cfe68730',
+    ),
+    'altorouter/altorouter' => 
+    array (
+      'pretty_version' => '2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '127f6e96998708a31ef32252985bea82e3b03888',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5232de97ee3b75b0360528dae24e73db49566ab1',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dc3063ba22c2a1fd2f45ed856374d79114998f91',
+    ),
+    'symfony/var-dumper' => 
+    array (
+      'pretty_version' => 'v5.2.8',
+      'version' => '5.2.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd693200a73fae179d27f8f1b16b4faf3e8569eba',
+    ),
+  ),
+);
