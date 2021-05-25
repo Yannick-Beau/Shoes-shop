aa3d506c8b825deabaf9fae377d221689900381c
Yannick-Beau <beauyannick@gmail.com>
2021-05-25 20:13:06 +0200

    challenge ok

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . "/../models/Type.php";
+
     class MainController
     {
         public function home() 
@@ -10,6 +12,33 @@
         public function legal() 
         {
             $this->show( "legal" );
+        }
+
+        public function test($routeVarInfos) 
+        {
+
+            // On instancie notre model
+            $brand = new Type();
+
+            // On appelle ensuite sa méthode find pour
+            // récupérer la bonne catégorie (sous la forme d'un objet Category)
+            $brand = $brand->find( $routeVarInfos['id'] );
+            // dump( $category );
+            // dump( $category->getName() );
+
+            // On a aussi la méthode findAll pour récupérer toutes les catégories
+            $brand->findAll();
+
+            $brand->findFooter_order();
+
+            // Je vais préparer un tableau des variables a transmettre à la vue
+            // pour lui permettre de les afficher
+            $viewVarsPofpof = [
+                'category' => $brand
+            ];
+
+            // Enfin je transmet ce tableau comme deuxième argument de show()
+            $this->show( "test", $viewVarsPofpof );
         }
 
         // Cette méthode n'est appellée QUE par le controleur via ses propres méthodes
